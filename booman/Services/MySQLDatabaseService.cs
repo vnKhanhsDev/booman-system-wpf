69c4396336a21ec186c85ae0c5e3d8077249f0bb
DinhDucKhanh <96609534+DinhDucKhanh@users.noreply.github.com>
2024-03-18 14:59:57 +0700

    up

--- booman/Services/MySQLDatabaseService.cs
+++ booman/Services/MySQLDatabaseService.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
-namespace booman.Services
+namespace booman.Service
 {
-    internal class MySQLDatabaseService
+    public class MySQLDatabaseService
     {
+        public MySqlConnection connection;
+        public string server;
+        public string database;
+        public string uid;
+        public string password;
+
+        public MySQLDatabaseService(string server, string database, string uid, string password)
+        {
+            this.server = server;
+            this.database = database;
+            this.uid = uid;
+            this.password = password;
+            string connectionString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public DataTable GetTableData(string tableName)
+        {
+            DataTable dataTable = new DataTable();
+
+            string query = $"SELECT * FROM {tableName}";
+
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            connection.Close();
+
+            return dataTable;
+        }
     }
 }
